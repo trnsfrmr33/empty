c1844fa0f705d645ae806dfa57bae84e5475d57e
James <58319265+trnsfrmr33@users.noreply.github.com>
2019-12-19 20:54:48 -0800

    Update caesar.c

--- caesar.c
+++ caesar.c
@@ -4,10 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(int argc, string argv[])
 {
-         
     //if command line isn't two strings
     if (argc != 2 || atoi(argv[1]) == 0)    
     {
@@ -17,26 +17,41 @@ int main(int argc, string argv[])
     }
    
     //prompt user for their plaintext
-    string s = get_string("Plaintext: ");
-    printf("%s\n", argv[1]);
+    string plain = get_string("Plaintext: ");
+   
+    //prints the output text and is ready for the next series of letters
+    printf("ciphertext: ");
+    
+    //sets an int to the value of argv[1]
+    int key = atoi(argv[1]);
     
     //iterate over each character of the plaintext using strlen
-    for (int i = 0; i < strlen(s); i++)
+    for (int i = 0; i < strlen(plain); i++)
     {
-        int c = (int) s[i];
+        //sets the value of c to the ith letter of the string s
+        int c = (int) plain[i];
+        
+        //rotate the letters by argv[1] positions 
         c = c + atoi(argv[1]);
-        printf("%c", c);
-        //if it is an uppercase,rotate it as an upper
         
-        //if it is lower, rotate as a lowercase
+        if (isupper(plain[i]))
+        {
+            printf("%c", (plain[i] - 'A' + key) % 26 + 'A');
+        }
         
-        //if it is neither, print the character as it is
-        
-        //print a new line
-       
-    }
-   printf("\n");
+        else if (islower(plain[i]))
+        {
+            printf("%c", (plain[i] - 'a' + key) % 26 + 'a');
+        }
+        else
+        {
+            printf("%c", plain[i]);    
+        }      
+    }    
+    //end of program, prints new line
+    printf("\n"); 
 }
-
-
+    
+    
+    
 
